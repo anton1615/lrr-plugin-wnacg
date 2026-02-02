a74a73b4691382f0938170f2a9ada587386c39cd
anton1615 <mailtoanton1615@gmail.com>
2026-02-02 16:20:32 +0800

    Fix Mojo success method and improve domain handling

--- Wnacg.pm
+++ Wnacg.pm
@@ -12,8 +12,8 @@ sub plugin_info {
         type         => "download",
         namespace    => "wnacg",
         author       => "Gemini CLI",
-        version      => "3.5",
-        description  => "Download from wnacg.com (Modern Mojo::UserAgent)",
+        version      => "3.6",
+        description  => "Download from wnacg.com (Fixed Mojo success method)",
         url_regex    => 'https?:\/\/(?:www\.)?wnacg\.(?:com|org|net).*(?:aid-|view-)\d+.*',
         parameters   => []
     );
@@ -24,31 +24,37 @@ sub provide_url {
     my $logger = get_plugin_logger();
     my $url = $lrr_info->{url};
 
-    $logger->info("--- Wnacg Mojo Triggered ---");
+    $logger->info("--- Wnacg Mojo v3.6 Triggered ---");
     
     # Normalize URL
     $url =~ s/photos-slide/photos-index/;
-    $url =~ s/wnacg\.(org|net)/wnacg.com/;
+    # Wnacg 台灣區常導向 .org，這裡統一處理
+    $url =~ s/wnacg\.(com|net)/wnacg.org/;
 
     my $ua = Mojo::UserAgent->new;
     $ua->transactor->name("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36");
+    $ua->max_redirects(3); # 處理可能的重定向
 
     # 獲取主頁
     my $tx = $ua->get($url);
-    if (my $res = $tx->success) {
+    my $res = $tx->result;
+
+    if ($res->is_success) {
         my $html = $res->body;
+        $logger->info("Fetched index page successfully.");
 
         # 策略 1: ZIP 下載
         if ($html =~ /href="(\/download-index-aid-(\d+)\.html)"/i) {
             my $aid = $2;
-            my $dl_page = "https://www.wnacg.com/download-index-aid-$aid.html";
-            my $dl_tx = $ua->get($dl_page);
-            if (my $dl_res = $dl_tx->success) {
+            my $dl_page = "https://www.wnacg.org/download-index-aid-$aid.html";
+            my $dl_res = $ua->get($dl_page)->result;
+            
+            if ($dl_res->is_success) {
                 my $dl_html = $dl_res->body;
                 if ($dl_html =~ /href="([^"]+\.zip[^"]*)"/i) {
                     my $zip_url = $1;
                     $zip_url = "https:" . $zip_url if $zip_url =~ /^\/\//;
-                    $logger->info("SUCCESS: $zip_url");
+                    $logger->info("SUCCESS: Found direct ZIP URL: $zip_url");
                     return ( url => $zip_url, title => "Wnacg ZIP $aid" );
                 }
             }
@@ -62,15 +68,16 @@ sub provide_url {
         }
         
         if (scalar @images > 0) {
-            $logger->info("SUCCESS: Images Found");
+            $logger->info("SUCCESS: Extracted " . scalar @images . " images.");
             return ( url_list => \@images, title => "Wnacg Archive" );
         }
     } else {
         my $err = $tx->error;
+        $logger->error("Mojo error: " . $err->{message});
         return ( error => "Mojo error: " . $err->{message} );
     }
 
-    return ( error => "No content found." );
+    return ( error => "No content found on Wnacg." );
 }
 
 1;
