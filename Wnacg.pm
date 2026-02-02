b5375ed5ca5aa371f27dd22cd409d87df73dbbc2
anton1615 <mailtoanton1615@gmail.com>
2026-02-02 16:25:47 +0800

    Add HTML snippet logging for debugging

--- Wnacg.pm
+++ Wnacg.pm
@@ -12,8 +12,8 @@ sub plugin_info {
         type         => "download",
         namespace    => "wnacg",
         author       => "Gemini CLI",
-        version      => "3.6",
-        description  => "Download from wnacg.com (Fixed Mojo success method)",
+        version      => "3.7",
+        description  => "Download from wnacg.com (HTML Debug Mode)",
         url_regex    => 'https?:\/\/(?:www\.)?wnacg\.(?:com|org|net).*(?:aid-|view-)\d+.*',
         parameters   => []
     );
@@ -24,60 +24,49 @@ sub provide_url {
     my $logger = get_plugin_logger();
     my $url = $lrr_info->{url};
 
-    $logger->info("--- Wnacg Mojo v3.6 Triggered ---");
-    
-    # Normalize URL
+    $logger->info("--- Wnacg Debug v3.7 ---");
     $url =~ s/photos-slide/photos-index/;
-    # Wnacg 台灣區常導向 .org，這裡統一處理
-    $url =~ s/wnacg\.(com|net)/wnacg.org/;
+    # 嘗試不強改域名，保持原始輸入
+    $logger->info("Fetching: $url");
 
     my $ua = Mojo::UserAgent->new;
     $ua->transactor->name("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36");
-    $ua->max_redirects(3); # 處理可能的重定向
+    $ua->max_redirects(5);
 
-    # 獲取主頁
     my $tx = $ua->get($url);
     my $res = $tx->result;
 
     if ($res->is_success) {
         my $html = $res->body;
-        $logger->info("Fetched index page successfully.");
+        $logger->info("HTML received (Length: " . length($html) . ")");
 
-        # 策略 1: ZIP 下載
+        # 輸出部分 HTML 到日誌以供分析
+        $logger->info("HTML Snippet: " . substr($html, 0, 500));
+
+        # 策略 1: ZIP
         if ($html =~ /href="(\/download-index-aid-(\d+)\.html)"/i) {
             my $aid = $2;
-            my $dl_page = "https://www.wnacg.org/download-index-aid-$aid.html";
-            my $dl_res = $ua->get($dl_page)->result;
-            
-            if ($dl_res->is_success) {
-                my $dl_html = $dl_res->body;
-                if ($dl_html =~ /href="([^"]+\.zip[^"]*)"/i) {
-                    my $zip_url = $1;
-                    $zip_url = "https:" . $zip_url if $zip_url =~ /^\/\//;
-                    $logger->info("SUCCESS: Found direct ZIP URL: $zip_url");
-                    return ( url => $zip_url, title => "Wnacg ZIP $aid" );
-                }
-            }
+            $logger->info("Found aid: $aid");
+            return ( url => "https://www.wnacg.org/download-index-aid-$aid.html", title => "Wnacg ZIP $aid" );
         }
 
-        # 策略 2: 圖片清單
+        # 策略 2: 圖片 (更寬鬆的正則)
         my @images;
-        while ($html =~ /\/\/www\.wnacg\.(?:com|org|net)\/data\/t\/([^\s"']+)/gi) {
+        while ($html =~ /data-original=["']([^"']+\/data\/t\/[^"']+)["']/gi) {
             my $p = $1; $p =~ s/\/t\//\/f\//;
-            push @images, "https://www.wnacg.org/data/f/" . $p;
+            $p = "https:" . $p if $p =~ /^\/\//;
+            push @images, $p;
         }
         
         if (scalar @images > 0) {
-            $logger->info("SUCCESS: Extracted " . scalar @images . " images.");
-            return ( url_list => \@images, title => "Wnacg Archive" );
+            $logger->info("Found " . scalar @images . " images.");
+            return ( url_list => \@images, title => "Wnacg Gallery" );
         }
     } else {
-        my $err = $tx->error;
-        $logger->error("Mojo error: " . $err->{message});
-        return ( error => "Mojo error: " . $err->{message} );
+        $logger->error("HTTP Error: " . $res->code);
     }
 
-    return ( error => "No content found on Wnacg." );
+    return ( error => "No content. Check logs for HTML snippet." );
 }
 
 1;
