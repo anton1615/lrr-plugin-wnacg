31189571adfe8ef54b0cb4e25039e34c90e08257
anton1615 <mailtoanton1615@gmail.com>
2026-02-02 15:18:56 +0800

    Fix imports and pass syntax check

--- Wnacg.pm
+++ Wnacg.pm
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 
 use LANraragi::Utils::Logging qw(get_logger);
-use LANraragi::Utils::Generic qw(get_html);
+use LANraragi::Utils::Generic; # 不使用匯入，改用全名調用
 
 sub plugin_info {
     return (
@@ -12,8 +12,8 @@ sub plugin_info {
         type         => "download",
         namespace    => "wnacg",
         author       => "Gemini CLI",
-        version      => "2.6",
-        description  => "Download from wnacg.com (Fixed Package Name)",
+        version      => "2.7",
+        description  => "Download from wnacg.com (Fixed Imports)",
         url_regex    => 'https?:\/\/(?:www\.)?wnacg\.(?:com|org|net)\/(?:photos-(?:index|slide)-aid-|view-)\d+.*',
         parameters   => []
     );
@@ -25,36 +25,30 @@ sub provide_url {
     my $url = $lrr_info->{url};
 
     $url =~ s/photos-slide/photos-index/;
-    $logger->info("Target: $url");
+    $logger->info("Processing Wnacg URL: $url");
 
-    my $html = get_html($url);
-    if (!$html) { return ( error => "No HTML content received from Wnacg." ); }
+    # 改用全名調用以避免匯入錯誤
+    my $html = LANraragi::Utils::Generic::get_html($url);
+    if (!$html) { return ( error => "Could not fetch index page." ); }
 
-    # 策略 1: 尋找直接下載 ZIP 頁面
     if ($html =~ /href="(\/download-index-aid-\d+\.html)"/i) {
         my $dl_page = "https://www.wnacg.org" . $1;
-        my $dl_html = get_html($dl_page);
+        my $dl_html = LANraragi::Utils::Generic::get_html($dl_page);
         if ($dl_html && $dl_html =~ /href="([^"]+\.zip[^"]*)"/i) {
             my $zip_url = $1;
             $zip_url = "https:" . $zip_url if $zip_url =~ /^\/\//;
-            $logger->info("Found direct ZIP URL: $zip_url");
             return ( url => $zip_url, title => "Wnacg ZIP" );
         }
     }
 
-    # 策略 2: 備援抓取圖片清單
     my @images;
     while ($html =~ /\/\/www\.wnacg\.(?:com|org|net)\/data\/t\/([^\s"']+)/gi) {
         my $p = $1; $p =~ s/\/t\//\/f\//;
         push @images, "https://www.wnacg.org/data/f/" . $p;
     }
     
-    if (scalar @images > 0) {
-        $logger->info("Falling back to image list: " . scalar @images . " images.");
-        return ( url_list => \@images, title => "Wnacg Archive" );
-    }
-
-    return ( error => "No images or ZIP found on Wnacg." );
+    return ( url_list => \@images, title => "Wnacg Archive" ) if scalar @images > 0;
+    return ( error => "No images or ZIP found." );
 }
 
 1;
