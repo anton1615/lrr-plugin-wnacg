0ee01b6b42bbf48657edfdf1b0e5a506b6a54f84
anton1615 <mailtoanton1615@gmail.com>
2026-02-02 16:58:48 +0800

    Standardize return format to List and use LRR UserAgent

--- Wnacg.pm
+++ Wnacg.pm
@@ -2,8 +2,8 @@ package LANraragi::Plugin::Download::Wnacg;
 
 use strict;
 use warnings;
+no warnings 'uninitialized';
 
-use Mojo::UserAgent;
 use LANraragi::Utils::Logging qw(get_plugin_logger);
 
 sub plugin_info {
@@ -12,25 +12,25 @@ sub plugin_info {
         type         => "download",
         namespace    => "wnacg",
         author       => "Gemini CLI",
-        version      => "3.9",
-        description  => "Download from wnacg.com (HashRef Return Fixed)",
-        url_regex    => 'https?:\/\/(?:www\.)?wnacg\.(?:com|org|net).*(?:aid-|view-)\d+.*',
-        parameters   => []
+        version      => "4.0",
+        description  => "Download from wnacg.com (Standard List Return)",
+        url_regex    => 'https?:\/\/(?:www\.)?wnacg\.(?:com|org|net).*(?:aid-|view-)\d+.*'
     );
 }
 
 sub provide_url {
-    my ($self, $lrr_info, @params) = @_;
+    shift;
+    my ($lrr_info, %params) = @_;
     my $logger = get_plugin_logger();
     my $url = $lrr_info->{url};
 
-    $logger->info("--- Wnacg Mojo v3.9 Triggered ---");
+    $logger->info("--- Wnacg Mojo v4.0 Triggered ---");
     
     # Normalize URL
     $url =~ s/photos-slide/photos-index/;
     
-    my $ua = Mojo::UserAgent->new;
-    $ua->transactor->name("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36");
+    # 使用 LRR 提供的 UserAgent (具備快取與 Session 管理)
+    my $ua = $lrr_info->{user_agent};
     $ua->max_redirects(5);
 
     my $tx = $ua->get($url);
@@ -40,25 +40,21 @@ sub provide_url {
         my $html = $res->body;
         $logger->info("Index page fetched. Size: " . length($html));
 
-        # 策略 1: 直接下載 ZIP
+        # 策略 1: ZIP 下載
         if ($html =~ m|href="(/download-index-aid-(\d+)\.html)"|i) {
             my $aid = $2;
-            # 獲取當前網域的 base
             my ($base) = $url =~ m|^(https?://[^/]+)|;
             my $dl_page = "$base/download-index-aid-$aid.html";
             
-            $logger->info("Checking download page: $dl_page");
-            my $dl_tx = $ua->get($dl_page);
-            my $dl_res = $dl_tx->result;
-            
+            my $dl_res = $ua->get($dl_page)->result;
             if ($dl_res->is_success) {
                 my $dl_html = $dl_res->body;
                 if ($dl_html =~ m|href="([^"]+\.zip[^"]*)"|i) {
                     my $zip_url = $1;
                     $zip_url = "https:" . $zip_url if $zip_url =~ m|^//|;
                     $logger->info("SUCCESS: Found ZIP URL: $zip_url");
-                    # 改用 HashRef 回傳
-                    return { url => $zip_url, title => "Wnacg ZIP $aid" };
+                    # 遵循官方規格回傳 download_url 列表
+                    return ( download_url => $zip_url );
                 }
             }
         }
@@ -72,16 +68,14 @@ sub provide_url {
         
         if (scalar @images > 0) {
             $logger->info("SUCCESS: Found " . scalar @images . " images.");
-            # 改用 HashRef 回傳
-            return { url_list => \@images, title => "Wnacg Gallery" };
+            # 如果是多圖下載，LRR 核心會處理 url_list
+            return ( url_list => \@images );
         }
     } else {
-        $logger->error("HTTP Error: " . $res->code);
-        return { error => "HTTP " . $res->code };
+        return ( error => "HTTP " . $res->code );
     }
 
-    $logger->error("No content found after parsing.");
-    return { error => "No content found on Wnacg." };
+    return ( error => "No content found on Wnacg." );
 }
 
 1;
